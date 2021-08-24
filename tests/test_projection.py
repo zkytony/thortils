4d2cc20a4a3c5d9398dbbe5ed765c5b48f3ab1dd
Kaiyu Zheng <kaiyutony@gmail.com>
2021-08-24 00:47:44 -0400

    clean up projection testing script

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -5,6 +5,7 @@ from thortils.controller import launch_controller, thor_controller_param
 from thortils.object import thor_object_poses
 from thortils.agent import thor_camera_pose
 from thortils.utils import clip
+from thortils import constants
 import numpy as np
 import open3d as o3d
 import random
@@ -12,7 +13,55 @@ from tqdm import tqdm
 from matplotlib import pyplot as plt
 
 
+def test_inverse_project_multiple_open3d(controller):
+    # Testing our method to use open3d to get point cloud
+    def do_step(controller, action, intrinsic, viz):
+        print(action)
+        controller.step(action=action)
+        event = controller.step(action="Pass")
+        camera_pose = thor_camera_pose(event, as_tuple=True)
+        pcd = pj.open3d_pcd_from_rgbd(event.frame, event.depth_frame, intrinsic,
+                                      camera_pose=camera_pose)
+        viz.add_geometry(pcd)
+
+    print("""test_inverse_project_multiple_open3d
+          This function will show a point cloud visualization
+          that is the result of fusing multiple point clouds
+          obtained from different robot poses; The RGBD->point cloud
+          is performed using open3d's internal method""")
+
+    # Obtain camera intrinsics (virtual camera, so can estimate intrinsics from fov).
+    fov = thor_controller_param(controller, "fieldOfView")
+    width = thor_controller_param(controller, "width")
+    height = thor_controller_param(controller, "height")
+    intrinsic = pj.pinhole_intrinsic(fov, width, height)
+
+    viz = o3d.visualization.Visualizer()
+    viz.create_window()
+
+    do_step(controller, "Pass", intrinsic, viz)
+    do_step(controller, "RotateLeft", intrinsic, viz)
+    do_step(controller, "MoveAhead", intrinsic, viz)
+    do_step(controller, "RotateRight", intrinsic, viz)
+    do_step(controller, "RotateRight", intrinsic, viz)
+    do_step(controller, "MoveAhead", intrinsic, viz)
+    do_step(controller, "RotateLeft", intrinsic, viz)
+    do_step(controller, "RotateLeft", intrinsic, viz)
+    do_step(controller, "RotateLeft", intrinsic, viz)
+    do_step(controller, "MoveAhead", intrinsic, viz)
+    do_step(controller, "MoveBack", intrinsic, viz)
+
+    opt = viz.get_render_option()
+    opt.show_coordinate_frame = True
+    viz.run()
+    viz.destroy_window()
+
+
 def test_inverse_project(controller):
+    print("""test_inverse_project
+          This function tests converting a single
+          RGBD image to a point cloud, using our method.""")
+
     # See if I can construct a point cloud using my method
     fov = thor_controller_param(controller, "fieldOfView")
     width = thor_controller_param(controller, "width")
@@ -61,10 +110,11 @@ def test_inverse_project_multiple(controller):
                                           depth_trunc=7, **params)
         return points, colors
 
-    print("This function will show a point cloud visualization"
-          "that is the result of fusing multiple point clouds"
-          "obtained from different robot poses; The RGBD->point cloud"
-          "is performed using thortil's native pcd_from_rgbd method")
+    print("""test_inverse_project_multiple
+          This function will show a point cloud visualization
+          that is the result of fusing multiple point clouds
+          obtained from different robot poses; The RGBD->point cloud
+          is performed using thortil's native pcd_from_rgbd method""")
 
     fov = thor_controller_param(controller, "fieldOfView")
     width = thor_controller_param(controller, "width")
@@ -97,6 +147,12 @@ def test_inverse_project_multiple(controller):
 
 
 def test_project(controller):
+    print("""test_project
+           This function tests converting a
+           point cloud to an RGBD image, using our method;
+           To verify the integrity of this RGBD image, ,
+           it will be converted to another point cloud and visualized.""")
+
     # Obtain camera intrinsics (virtual camera, so can estimate intrinsics from fov).
     fov = thor_controller_param(controller, "fieldOfView")
     width = thor_controller_param(controller, "width")
@@ -137,6 +193,7 @@ def test_project(controller):
 
 
 def rgbd_to_pcd_double(event, intrinsic, camera_pose):
+    """Helper method for test_project_multiple"""
     # For testing; convert rgb and depth in the event to point cloud,
     # then convert this pcd to rgb, then convert this rgb to pcd.
     # This is to test the correctness of our rgbd_from_pcd method.
@@ -148,9 +205,6 @@ def rgbd_to_pcd_double(event, intrinsic, camera_pose):
                                         depth_scale=1.0,
                                         depth_trunc=7, downsample=0.8,
                                         show_progress=True)
-    # points1 = np.asarray(pcd1.points)
-    # colors1 = np.asarray(pcd1.colors) * 255
-    # Using out rgbd_from_pcd method
     color2, depth2 = pj.rgbd_from_pcd(points1, colors1,
                                       intrinsic, camera_pose, show_progress=True)
     # Get the point cloud again and render it
@@ -162,13 +216,21 @@ def rgbd_to_pcd_double(event, intrinsic, camera_pose):
 
 def test_project_multiple(controller):
     def pcd_after(controller, action, intrinsic, **params):
+        """returns pcd after given action is executed"""
         controller.step(action=action)
         event = controller.step(action="Pass")
         camera_pose = thor_camera_pose(event, as_tuple=True)
         return rgbd_to_pcd_double(event, intrinsic, camera_pose)
 
-    print("This test currently fails. The point clouds from different generated RGBD images"\
-          "do not share the same coordinate system.")
+    print("""test_project_multiple
+          This function will show a point cloud visualization
+          that is the result of fusing the point clouds from
+          multiple poses; At each pose, (1) obtain RGBD image,
+          (2) obtain point cloud given the RGBD image
+          (3) Obtain RGBD image given point cloud, using **our pj.rgbd_from_pcd**
+          method; (4) Obtain point cloud again but using the generated
+          RGBD image from step 3. This verifies that both
+          projection and inverse projection work properly""")
 
     fov = thor_controller_param(controller, "fieldOfView")
     width = thor_controller_param(controller, "width")
@@ -196,8 +258,9 @@ def test_project_multiple(controller):
 
 
 if __name__ == "__main__":
-    controller = launch_controller({'scene': "FloorPlan1"})
-    # test_inverse_project(controller)
-    # test_inverse_project_multiple(controller)
-    # test_project(controller)
+    controller = launch_controller({**constants.CONFIG, **{'scene': "FloorPlan1"}})
+    test_inverse_project_multiple_open3d(controller)
+    test_inverse_project(controller)
+    test_inverse_project_multiple(controller)
+    test_project(controller)
     test_project_multiple(controller)
